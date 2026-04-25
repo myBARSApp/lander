49b1e92e61325a3f56f79d756e26d7fb6c347c5b
myBARSApp <william@mybars.app>
2026-04-25 15:37:40 -0400

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+
+import Hero from "../components/Hero";
+import Problem from "../components/Problem";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Problem />
+      <Footer />
+    </main>
+  );
+}
